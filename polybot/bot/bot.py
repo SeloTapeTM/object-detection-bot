abbfc4b8e76589e2ce741de0073a57174af7e77d
OmerDaniel <omer.daniel.1999@gmail.com>
2024-01-12 19:01:32 +0200

    Updated bot.py tidy up and removed excess

--- polybot/bot/bot.py
+++ polybot/bot/bot.py
@@ -112,14 +112,6 @@ class Bot:
         self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
 
 
-class QuoteBot(Bot):
-    def handle_message(self, msg):
-        logger.info(f'Incoming message: {msg}')
-
-        if msg["text"] != 'Please don\'t quote me':
-            self.send_text_with_quote(msg['chat']['id'], msg["text"], quoted_msg_id=msg["message_id"])
-
-
 class ImageProcessingBot(Bot):
     def __init__(self, token, telegram_chat_url, bucket_name, yolo5_cont_name):
         super().__init__(token, telegram_chat_url, bucket_name, yolo5_cont_name)
@@ -156,14 +148,14 @@ class ImageProcessingBot(Bot):
                     logger.info("Received photo with wrong caption.")
                     response = (f'Oh no!\nThe filter that you\'ve specified does not exist yet.\n Please send it again '
                                 f'with the fiter you want to apply in the \"caption\" of the picture from the list of f'
-                                f'ilters.\n\nFor the list of available filters you can type \"/filters\"')
+                                f'ilters.\n\nFor the list of available filters you can type \"/actions\"')
                     self.send_text(msg['chat']['id'], response)
 
             else:
                 logger.info("Received photo without a caption.")
                 response = (f'Oh no!\nThe photo that you\'ve sent does not contain any filters in the caption.\n Please'
                             f' send it again with the fiter you want to apply in the \"caption\" of the picture.\n\nFor'
-                            f' the list of available filters you can type \"/filters\"')
+                            f' the list of available filters you can type \"/actions\"')
                 self.send_text(msg['chat']['id'], response)
         elif "text" in msg:
             message = msg['text'].lower()
@@ -179,7 +171,7 @@ class ImageProcessingBot(Bot):
                             f' right now you can type \"/actions\".')
                 self.send_text(msg['chat']['id'], response)
             elif '/actions' in message:
-                logger.info("Received text with command /filters.")
+                logger.info("Received text with command /actions.")
                 response = (f'The list of actions\\filters is:\n\nBlur - Blurs the image.\nContour - Shows only outline'
                             f's.\nSalt n Pepper - Randomly place white and black pixels over the picture.\nSegment -'
                             f' Makes all the bright parts white and all the dark parts black.\n\nNEW!!!\nDetect - detec'
@@ -334,71 +326,3 @@ class ImageProcessingBot(Bot):
             self.send_text(msg['chat']['id'], f'Detected objects: \n{counted_response_msg}')
 
         self.processing_completed = True
-
-
-class ObjectDetectionBot(Bot):
-    def __init__(self, token, telegram_chat_url):
-        super().__init__(token, telegram_chat_url)
-        self.s3_client = boto3.client('s3')
-
-    def handle_message(self, msg):
-        logger.info(f'Incoming message: {msg}')
-
-        if self.is_current_msg_photo(msg):
-            photo_path = self.download_user_photo(msg)
-            bucket = "omers3bucketpublic"
-            img_name = f'tg-photos/{photo_path}'
-            upload_response = upload_file(photo_path, bucket, img_name)  # upload the photo to S3
-            if not upload_response:
-                raise ClientError
-            else:
-                logger.info(f'Successfully uploaded {photo_path} to {bucket}/{img_name}')
-            logger.info(f'before request')
-            response_code, json_response = yolo5_request(img_name)  # send a request to the `yolo5` service for prediction
-            logger.info(f'after request')
-            if response_code is None:
-                self.send_text(msg['chat']['id'], 'No detections found with your img.')
-            else:
-                counted_response_msg = count_prediction_msg(json_response)
-                self.send_text(msg['chat']['id'], f'Detected objects: \n{counted_response_msg}')
-
-        elif "text" in msg:
-            message = msg['text'].lower()
-            if '/start' in message:
-                logger.info("Received text with command /start.")
-                response_code = (f'Oh, Hi there!\nWelcome to Omer\'s Image Processing Bot!\n\nFor information on how to'
-                                 f' use the bot type \"/help\".\nFor the list of actions type \"/actions\".')
-                self.send_text(msg['chat']['id'], response_code)
-            elif '/help' in message:
-                logger.info("Received text with command /help.")
-                response_code = (f'In order to use the bot properly you should send any photo, and in the \"caption'
-                                 f'\" type in the name of the filter you want to apply.\n\nFor the list of filters available'
-                                 f' right now you can type \"/filters\".')
-                self.send_text(msg['chat']['id'], response_code)
-            elif '/filters' in message:
-                logger.info("Received text with command /filters.")
-                response_code = (f'The list of filters is:\n\nBlur - Blurs the image.\nContour - Shows only outlines.\n'
-                                 f'Salt n Pepper - Randomly place white and black pixels over the picture.\nSegment -'
-                                 f' Makes all the bright parts white and all the dark parts black.\n\nFor information on how'
-                                 f' to use the filters you can type \"/help\".')
-                self.send_text(msg['chat']['id'], response_code)
-            elif 'i hate you' in message:
-                logger.info("Received text that says \"i hate you\".")
-                response_code = f'You\'ve insulted me! And that is not nice at all.. You should be ashamed of yourself.'
-                self.send_text(msg['chat']['id'], response_code)
-            elif 'i love you' in message:
-                logger.info("Received text that says \"i love you\".")
-                response_code = f'Awwww, I love you too! <3 XOXO'
-                self.send_text(msg['chat']['id'], response_code)
-            elif 'supercalifragilisticexpialidocious' in message:
-                logger.info("Received easteregg.")
-                response_code = f'https://boulderbugle.com/super-secret-easter-egg-39tz7pni'
-                self.send_text(msg['chat']['id'], response_code)
-            elif 'supercalifragilisticexpialodocious' in message:
-                logger.info("Received easteregg.")
-                response_code = f'https://boulderbugle.com/super-secret-easter-egg-39tz7pni'
-                self.send_text(msg['chat']['id'], response_code)
-            else:
-                response_code = (f'What you\'ve typed (\"{msg["text"]}\") is not a recognisable command.\n\nTry typing '
-                                 f'\"/help\"')
-                self.send_text(msg['chat']['id'], response_code)
